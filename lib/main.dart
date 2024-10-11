7d3b3690ddf1504076e5b508801bcbf0a58c5241
Syimyk <105375242+abdybekusyimyk@users.noreply.github.com>
2024-10-11 14:35:32 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:mbank/modules/home_view.dart';
 
 void main() {
   runApp(const MyApp());
+  print('test');
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  
 
   @override
   Widget build(BuildContext context) {
